37f44de243422cd482833d8ec0d75e5310b58274
Artwell-Runyararo <runyararo7artwell@gmail.com>
2024-08-28 16:04:39 +0200

    posting

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 from core.config import settings
+from apis.base import api_router
 
 
 # def create_tables():
 #     Base.metadata.create_all(bind=engine)
 
 
+def include_router(app):
+    app.include_router(api_router)
+
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
     # create_tables()
+    include_router(app)
     return app
 
 
--- schemas/user.py
+++ schemas/user.py
@@ -3,4 +3,13 @@ from pydantic import BaseModel, EmailStr, Field
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: str = Field(...,min_length=4)
+    password: str = Field(...,min_length=8)
+
+
+class ShowUser(BaseModel):
+    id:int
+    email: EmailStr
+    is_active: bool
+
+    class Config():
+        orm_mode = True
